4461441298aa14cb4c7b40f073f13aa2a5ee35fa
thang241190 <thangnguyen24111990@gmail.com>
2021-04-30 12:23:16 +0300

    Finish ex6.3

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import {
+	Container,
+	Paper,
+	Typography,
+	TextField,
+	Select,
+	MenuItem,
+	Box,
+} from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		margin: theme.spacing(2),
+		padding: theme.spacing(2, 1),
+	},
+	box: {
+		display: "flex",
+		justifyContent: "space-between",
+	},
+	input: {
+		height: 30,
+	},
+}))
+
+const data = {
+	en: {
+		REVERSER: "Reverser",
+		REVERSED: "Reversed:",
+		INPUT: "Input:",
+	},
+	fi: {
+		REVERSER: "Peruutin",
+		REVERSED: "Päinvastaiseksi:",
+		INPUT: "Tulo:",
+	},
+}
+
+export default function App() {
+	const classes = useStyles()
+	const [text, setText] = useState()
+	const [lang, setLang] = useState("en")
+	const handleChangeText = (e) => {
+		const newText = e.target.value.split("").reverse().join("")
+		setText(newText)
+	}
+	const handleLanguageChange = (e) => {
+		setLang(e.target.value)
+	}
+	return (
+		<div className={classes.root}>
+			<Container maxWidth='sm'>
+				<Paper elevation={2} className={classes.paper}>
+					<Box className={classes.box}>
+						<Typography variant='h6'>
+							{data[lang].REVERSER}
+						</Typography>
+						<Select
+							labelId='demo-simple-select-label'
+							id='demo-simple-select'
+							value={lang}
+							onChange={handleLanguageChange}>
+							<MenuItem value='en'>English</MenuItem>
+							<MenuItem value='fi'>Suomi</MenuItem>
+						</Select>
+					</Box>
+					<Box className={classes.box}>
+						<Typography variant='h6'>{data[lang].INPUT}</Typography>
+						<Typography variant='h6'>
+							{data[lang].REVERSED}
+						</Typography>
+					</Box>
+					<Box className={classes.box}>
+						<TextField
+							InputProps={{
+								className: classes.input,
+							}}
+							onChange={handleChangeText}
+							variant='outlined'
+						/>
+						<Typography variant='h6'>{text}</Typography>
+					</Box>
+				</Paper>
+			</Container>
+		</div>
+	)
+}
